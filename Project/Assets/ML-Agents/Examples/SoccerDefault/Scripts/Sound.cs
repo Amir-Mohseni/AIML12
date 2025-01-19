fce4f0053ce0f39f58b508544c952e555dbc0e2d
AleksanderMasojc <olekmasojc1@gmail.com>
2025-01-19 11:56:24 +0100

    new branch

---
+++ Project/Assets/ML-Agents/Examples/SoccerDefault/Scripts/Sound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public static class Sound
+{
+    static List<Rigidbody> objects = new List<Rigidbody>();
+
+    static void AddObject(Rigidbody t)
+    {
+        objects.Add(t);
+    }
+
+    static List<Rigidbody> getObjects(Rigidbody curr)
+    {
+        List<Rigidbody> res = new List<Rigidbody>();
+        foreach (Rigidbody t in objects)
+        {
+            if (curr != t && t.velocity.magnitude > 0) res.Add(t);
+        }
+        return res;
+    }
+}
